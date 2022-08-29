1cf09edc54ba140d52b1718b9ff718e399935c8b
y0ri <48780395+y0ri@users.noreply.github.com>
2022-08-29 15:53:43 +0200

    Update F1.py

--- receptor_similarity/code/F1.py
+++ receptor_similarity/code/F1.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-This script contains the code behind the results in F1 in manuscript xxx
+This script contains the code behind the results in F1 in manuscript 
+NEUROTRANSMITTER TRANSPORTER/RECEPTOR CO-EXPRESSION SHARES ORGANIZATIONAL TRAITS WITH BRAIN STRUCTURE AND FUNCTION
+https://doi.org/10.1101/2022.08.26.505274
 """
 from scipy.stats import zscore
 from brainspace.gradient import GradientMaps
