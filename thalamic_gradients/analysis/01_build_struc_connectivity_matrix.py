adac54fa07478d04d275bcc8c7ec6304f1b846d3
John-Alexandra <78356097+John-Alexandra@users.noreply.github.com>
2024-02-01 17:30:48 +0100

    analysis scripts preprint version

---
+++ thalamic_gradients/analysis/01_build_struc_connectivity_matrix.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 10:35:43 2022
+
+@author: ajohn
+
+1. import probabilisic tractography data
+2. build structural connectivity matrix M x N matrix (M - thalamic voxels, N - 200 Schaefer parcels) 
+   - iterate over thalamic voxels per schaefer parcel and put as columns inside matrix (number of samples seeded from that voxel reaching the relevant target mask)
+
+data set: mica mics
+schaefer parcellation: 200
+left hemisphere and right hemisphere separately (hard coded)
+"""
+
+#import modules
+import numpy as np
+import nibabel as nb
+
+""" left hemisphere """
+
+sublist=np.arange(1,51,1) #subject list
+schaefer_parcels_lh=np.arange(1,101,1)                  # left hemisphere
+
+# paths to save results
+save_to="/data/p_02666/Project1_thalamus/structural_connectivity/parcels_200/struc_conn_matrix_lh_sub.npy"
+avg_save_to="/data/p_02666/Project1_thalamus/structural_connectivity/parcels_200/struc_conn_matrix_lh_avg.npy"
+
+# import refined thalamus mask as reference
+thala_ref_lh_path="/data/p_02666/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_lh_refined.nii.gz"
+thala_ref_lh=nb.load(thala_ref_lh_path).get_fdata()
+
+# collect indices of voxels where mask = 1 
+idx_l=np.where(thala_ref_lh==1)        
+#counts number of voxels in thalamic_mask              
+vox_lh=np.count_nonzero(thala_ref_lh)                   # 1068 voxels
+
+# create array to stack connectivity matrices of all subjects
+conn_matrix_stack=np.zeros((vox_lh,100,50))    
+
+# CREATE CONNECTIVITY MATRIX based on probtrackx_output
+# iterate over subjects
+for s, sub in enumerate(sublist):
+    print("sub_HC0{:02d}".format(sub))
+    # iterate over each output file (one nifti per parcel) 
+    for i, parcel in enumerate(schaefer_parcels_lh): 
+        path="/data/p_02666/mica-mics/probtrackx_out/sub_HC0{:02d}/ses-01/dwi/left_hem/seeds_to_space-MNI_atlas-schaefer-200_parcel_{}.nii.gz".format(sub,parcel)
+        thala_to_parcel=nb.load(path).get_fdata()       # load output file as array
+        values=thala_to_parcel[idx_l]                   # returns all values at location where thalamus mask = 1
+        conn_matrix_stack[:,i,s]=values                 # puts the values as colum inside array, stack subjects
+np.save(save_to, conn_matrix_stack)
+#conn_matrix_stack=np.load(save_to)
+
+# AVERAGE connectivity matrix across subjects
+conn_matrix_l = np.mean(conn_matrix_stack, axis=2) 
+
+# NORMALIZE columnwise 
+for i in range(100):                                    # iterate over columns ( ~ parcels)
+    max_val=np.max(conn_matrix_l[:,i])                  # seach for max per column 
+    conn_matrix_l[:,i]=conn_matrix_l[:,i]/max_val       # divide every value in column by max value of this columns (to set it to 1)
+
+np.save(avg_save_to, conn_matrix_l)
+
+
+
+""" right hemisphere """
+
+sublist=np.arange(1,51,1) #subject list
+schaefer_parcels_rh=np.arange(101,201,1)               # right hemisphere
+
+# paths to save results
+save_to="/data/p_02666/Project1_thalamus/structural_connectivity/parcels_200/struc_conn_matrix_rh_sub.npy"
+avg_save_to="/data/p_02666/Project1_thalamus/structural_connectivity/parcels_200/struc_conn_matrix_rh_avg.npy"
+
+# import refined thalamus mask as reference
+thala_ref_rh_path="/data/p_02666/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_rh_refined.nii.gz"
+thala_ref_rh=nb.load(thala_ref_rh_path).get_fdata()
+
+# collect indices of voxels where mask = 1 
+idx_r=np.where(thala_ref_rh==1)        
+#counts number of voxels in thalamic_mask              
+vox_rh=np.count_nonzero(thala_ref_rh)                 # 1029 voxels
+
+# create array to stack connectivity matrices of all subjects
+conn_matrix_stack=np.zeros((vox_rh,100,50))    
+
+# CREATE CONNECTIVITY MATRIX based on probtrackx_output
+# iterate over subjects
+for s, sub in enumerate(sublist):
+    print("sub_HC0{:02d}".format(sub))
+    # iterate over each output file (one nifti per parcel) 
+    for i, parcel in enumerate(schaefer_parcels_rh): 
+        path="/data/p_02666/mica-mics/probtrackx_out/sub_HC0{:02d}/ses-01/dwi/right_hem/seeds_to_space-MNI_atlas-schaefer-200_parcel_{}.nii.gz".format(sub,parcel)
+        thala_to_parcel=nb.load(path).get_fdata()       # load output file as array
+        values=thala_to_parcel[idx_r]                   # returns all values at location where thalamus mask = 1
+        conn_matrix_stack[:,i,s]=values                 # puts the values as colum inside array, stack subjects
+np.save(save_to, conn_matrix_stack)
+#conn_matrix_stack=np.load(save_to)
+
+
+# AVERAGE connectivity matrix across subjects
+conn_matrix_r = np.mean(conn_matrix_stack, axis=2) 
+
+# NORMALIZE columnwise 
+for i in range(100):                                    # iterate over columns ( ~ parcels)
+    max_val=np.max(conn_matrix_r[:,i])                  # seach for max per column 
+    conn_matrix_r[:,i]=conn_matrix_r[:,i]/max_val       # divide every value in column by max value of this columns (to set it to 1)
+
+np.save(avg_save_to, conn_matrix_r)
---
+++ thalamic_gradients/analysis/11_Supp_QC_scov.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 15 13:44:25 2023
+
+@author: ajohn
+
+main question: we have differences in our structural covariance patterns between left and right and we want to check whether there is a left-right bias in the files
+1. use mean T1q across layers 
+2. correlating between left voxelwise thalamic T1q and wholebrain cortex parcel T1q
+3. links this to left structural connectivity gradients by correlating columns (parcels) of structural covariance with G1 and G2 gradient loadings 
+4. same for right hem
+"""
+#import modules
+import numpy as np
+import matplotlib.pyplot as plt
+import nibabel as nb
+import pandas as pd
+
+sublist=np.arange(1,51,1)  
+"""
+1) calculate structural covariance of left thalamus with whole brain
+"""
+
+### left thalamus -> whole brain
+thala_ref_lh=nb.load("/data/p_02666/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_lh_refined.nii.gz").get_fdata()
+nr_v=np.count_nonzero(thala_ref_lh)
+allsub_T1q_l=np.zeros((nr_v,50)) 
+idx_l=np.where(thala_ref_lh==1)
+
+#iterate over mp2rage in mni space of each subject
+for s,sub in enumerate(sublist):
+    print(sub)
+    #load in mprage_T1map -> 2mm 1mm?
+    T1q=nb.load("/data/p_02666/mica-mics/space_mp2rage_T1map_to_MNI/SUB_HC0{:02d}/fnirt_mp2rage_T1map_to_MNI2mm.nii.gz".format(sub)).get_fdata()
+    values=T1q[idx_l]     #returns all values at location where thalamus mask is 1
+    allsub_T1q_l[:,s]=values 
+    
+       
+###correlate voxelwise####
+
+#correlate between gradient T1q voxels and schaefer parcels  -> per layer
+intensity_profiles=np.load("/data/p_02666/Project1_thalamus/structural_covariance/cortex_intensity_profiles/intensity_profiles_mean_schaefer200_allsub.npy")
+parcels=np.arange(0,200,1)  #both hemisphere
+    
+#corr_coefficients -> retunrs Rvalues (Pearson)
+corr_vx=np.zeros((nr_v,200))
+vox_nr=np.arange(0,nr_v,1) ##?
+
+for v, vox in enumerate(vox_nr):
+    voxel=allsub_T1q_l[v,:]
+    
+    for p,parc in enumerate(parcels):
+        x=np.corrcoef(voxel,intensity_profiles[p,:])[0,1]
+        corr_vx[v,p]=x   
+            
+#->scov
+#load gradient vector
+grad=np.load("/data/p_02666/Project1_thalamus/structural_connectivity/parcels_200/gradients_lh.npy")
+# 1. Gradient
+R_values=np.zeros((200,1))
+
+#iterate over columns
+for p,parc in enumerate(parcels):
+    R=np.corrcoef(corr_vx[:,p],grad[:,0])[0,1]
+    #collect R values per parcel
+    R_values[p]=R
+
+np.save("/data/p_02666/Project1_thalamus/structural_covariance/QC/R_values_left_th_whole_cortex_g1.npy", R_values)
+
+# 2. Gradient
+R_values=np.zeros((200,1))
+
+#iterate over columns
+for p,parc in enumerate(parcels):
+    R=np.corrcoef(corr_vx[:,p],grad[:,1])[0,1]
+    #collect R values per parcel
+    R_values[p]=R
+
+np.save("/data/p_02666/Project1_thalamus/structural_covariance/QC/R_values_left_th_whole_cortex_g2.npy", R_values)
+
+#####################################################################
+### right thalamus -> whole brain
+thala_ref_rh=nb.load("/data/p_02666/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_rh_refined.nii.gz").get_fdata()
+nr_v=np.count_nonzero(thala_ref_rh)
+allsub_T1q=np.zeros((nr_v,50)) 
+idx_r=np.where(thala_ref_rh==1)
+
+#iterate over mp2rage in mni space of each subject
+for s,sub in enumerate(sublist):
+    print(sub)
+    #load in mprage_T1map -> 2mm 1mm?
+    T1q=nb.load("/data/p_02666/mica-mics/space_mp2rage_T1map_to_MNI/SUB_HC0{:02d}/fnirt_mp2rage_T1map_to_MNI2mm.nii.gz".format(sub)).get_fdata()
+    values=T1q[idx_r]     #returns all values at location where thalamus mask is 1
+    allsub_T1q[:,s]=values 
+
+###correlate voxelwise####
+
+#correlate between gradient T1q voxels and schaefer parcels  -> per layer
+intensity_profiles=np.load("/data/p_02666/Project1_thalamus/structural_covariance/cortex_intensity_profiles/intensity_profiles_mean_schaefer200_allsub.npy")
+parcels=np.arange(0,200,1)  #left hemisphere
+    
+#corr_coefficients -> retunrs Rvalues (Pearson)
+corr_vx=np.zeros((nr_v,200))
+vox_nr=np.arange(0,nr_v,1) ##?
+
+#iterate over voxel
+for v, vox in enumerate(vox_nr):
+    voxel=allsub_T1q[v,:]
+    
+    for p,parc in enumerate(parcels):
+        x=np.corrcoef(voxel,intensity_profiles[p,:])[0,1]
+        corr_vx[v,p]=x   
+#->scov
+
+#load gradient vector
+grad=np.load("/data/p_02666/Project1_thalamus/structural_connectivity/parcels_200/gradients_rh.npy")
+# 1. Gradient
+R_values=np.zeros((200,1))
+
+#iterate over columns
+for p,parc in enumerate(parcels):
+    R=np.corrcoef(corr_vx[:,p],grad[:,0])[0,1]
+    #collect R values per parcel
+    R_values[p]=R       
+
+np.save("/data/p_02666/Project1_thalamus/structural_covariance/QC/R_values_right_th_whole_cortex_g1.npy", R_values)
+
+# 2. Gradient
+R_values=np.zeros((200,1))
+
+#iterate over columns
+for p,parc in enumerate(parcels):
+    R=np.corrcoef(corr_vx[:,p],grad[:,1])[0,1]
+    #collect R values per parcel
+    R_values[p]=R       
+
+np.save("/data/p_02666/Project1_thalamus/structural_covariance/QC/R_values_right_th_whole_cortex_g2.npy", R_values)
+
+# also liegt rechts mit links besser als rechts mit rechts 
+
+
+
+
+
+
+
+
