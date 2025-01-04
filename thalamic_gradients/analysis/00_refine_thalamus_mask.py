4bab9303a8510f4cd213da2a6a7f4c2f0bf8a0db
John-Alexandra <78356097+John-Alexandra@users.noreply.github.com>
2025-01-04 16:07:47 +0100

    updatet paths

--- thalamic_gradients/analysis/00_refine_thalamus_mask.py
+++ thalamic_gradients/analysis/00_refine_thalamus_mask.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jul  8 12:14:04 2022
-- script refines thalamus mask by manually thresholding
+- script refines thalamus mask (from harvard-oxford atlas integrated in FSL) by manually thresholding 
 
 @author: ajohn
 """
@@ -13,18 +13,19 @@ import numpy as np
 
 """ left hemisphere """
 
-img=nb.load("/data/p_02666/mica-mics/space_mp2rage_T1map_to_MNI/SUB_HC001/fnirt_uni_T1map_to_MNI2mm_iout.nii.gz").get_fdata()
-thala=nb.load("/data/p_02666/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_lh.nii.gz").get_fdata()
+img=nb.load("/mica-mics/space_mp2rage_T1map_to_MNI/SUB_HC001/fnirt_uni_T1map_to_MNI2mm_iout.nii.gz").get_fdata()
+thala=nb.load("/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_lh.nii.gz").get_fdata()
 
-
+# get indices
 idx_l=zip(*np.where(thala==1))
 
+# set manuel threshhold
 for i, idx in enumerate(idx_l):
-    if img[idx] < 1800000:       #manuell set threshhold
+    if img[idx] < 1800000:       
         thala[idx]=0
-    
 
-img=nb.load("/data/p_02666/mica-mics/space_mp2rage_T1map_to_MNI/SUB_HC001/fnirt_uni_T1map_to_MNI2mm_iout.nii.gz")
+# save refined mask as nifti    
+img=nb.load("/mica-mics/space_mp2rage_T1map_to_MNI/SUB_HC001/fnirt_uni_T1map_to_MNI2mm_iout.nii.gz") #only for header info
 clipped_img = nb.Nifti1Image(thala, img.affine, img.header)
 nb.save(clipped_img, '/data/p_02666/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_lh_refined.nii.gz')
 
@@ -34,17 +35,18 @@ nb.save(clipped_img, '/data/p_02666/mica-mics/thalamus_space_mni/space-MNI125_at
 
 """ right hemisphere """
 
-img=nb.load("/data/p_02666/mica-mics/space_mp2rage_T1map_to_MNI/SUB_HC001/fnirt_uni_T1map_to_MNI2mm_iout.nii.gz").get_fdata()
-thala=nb.load("/data/p_02666/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_rh.nii.gz").get_fdata()
+img=nb.load("/mica-mics/space_mp2rage_T1map_to_MNI/SUB_HC001/fnirt_uni_T1map_to_MNI2mm_iout.nii.gz").get_fdata()
+thala=nb.load("/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_rh.nii.gz").get_fdata()
 
-
+# get indices
 idx_l=zip(*np.where(thala==1))
 
+# set manuel threshhold
 for i, idx in enumerate(idx_l):
-    if img[idx] < 1800000:       #manuell set threshhold
+    if img[idx] < 1800000:       
         thala[idx]=0
     
-
-img=nb.load("/data/p_02666/mica-mics/space_mp2rage_T1map_to_MNI/SUB_HC001/fnirt_uni_T1map_to_MNI2mm_iout.nii.gz")
+# save refined mask as nifti  
+img=nb.load("/mica-mics/space_mp2rage_T1map_to_MNI/SUB_HC001/fnirt_uni_T1map_to_MNI2mm_iout.nii.gz") #only for header info
 clipped_img = nb.Nifti1Image(thala, img.affine, img.header)
-nb.save(clipped_img, '/data/p_02666/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_rh_refined.nii.gz')
+nb.save(clipped_img, '/mica-mics/thalamus_space_mni/space-MNI125_atlas-thalamus_rh_refined.nii.gz')
